5ee9aef254f07a7d188c9ec8f7847e3f19e44d19
Joy Wan <wanzhiyu@outlook.com>
2019-08-11 21:38:29 +0800

    基本完成二值化，虽然结果是十分丑陋的……

--- binarize_imgs.m
+++ binarize_imgs.m
@@ -1,3 +1,9 @@
-function [] = binarize_imgs()
-
+function [] = binarize_imgs(Path_OriginImgs, Path_BinarizedImgs)
+    imgs_name = dir(fullfile(Path_OriginImgs, '/*.bmp'));
+    for i = 1:length(imgs_name)
+        img_path = fullfile(imgs_name(i).folder,imgs_name(i).name);
+        img_gray = rgb2gray(imread(img_path));
+        img_dithered = Floyd_Steinberg_Dithering(img_gray);
+        imwrite(img_dithered, fullfile(Path_BinarizedImgs, imgs_name(i).name));
+    end
 end
--- bool2grey.m
+++ bool2grey.m
@@ -1,5 +1,5 @@
 function img_grey = bool2grey(img_bool, grey_percent)
-%MATRIX2GREY 将bool矩阵根据传入的灰度比例转换为灰度图像并显示，返回灰度矩阵
+%bool2GREY 将bool矩阵根据传入的灰度比例转换为灰度图像并显示，返回灰度矩阵
 
 img_grey = img_bool .* grey_percent;
 % imshow(img_grey);
